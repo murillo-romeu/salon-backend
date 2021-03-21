07e7671b84a358c38349369e08cd2836e01b066f
Murillo Romeu <murillo@sysconsulting.com.br>
2021-03-21 16:39:26 -0300

    Eslint

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,8 @@
+/* eslint-disable no-console */
 import express from 'express';
 
 const app = express();
 
 app.listen(3333, () => {
-  console.log('🚀 Server started on port 3333!')
-})
+  console.log('🚀 Server started on port 3333!');
+});
