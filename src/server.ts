91354213f296d4d9364da8a89ecf4e9a4ea75c3b
Murillo Romeu <murillo@sysconsulting.com.br>
2021-03-21 16:48:57 -0300

    eslint, impor packages

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,9 @@
 /* eslint-disable no-console */
 import express from 'express';
+import routes from './routes';
 
 const app = express();
+app.use(routes);
 
 app.listen(3333, () => {
   console.log('🚀 Server started on port 3333!');
