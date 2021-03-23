4e49d50991bf62c72a78e948a26c7de03b8c46df
Murillo Romeu <murillo@sysconsulting.com.br>
2021-03-23 14:25:41 -0300

    debug config

--- src/routes/index.ts
+++ src/routes/index.ts
@@ -2,6 +2,15 @@ import { Router } from 'express';
 
 const routes = Router();
 
-routes.get('/', (request, response) => response.json({ message: 'Hello World' }));
+routes.get('/users', (request, response) => {
+  const { name, password } = request.body;
+
+  const user = {
+    name,
+    password,
+  };
+
+  return response.json({ message: user });
+});
 
 export default routes;
