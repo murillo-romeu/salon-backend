16f5a34b68b6973d78b71cf0df9d0dc038611f1c
Murillo Romeu <murillo@sysconsulting.com.br>
2021-03-25 23:59:10 -0300

    Upload avatar

--- src/routes/users.routes.ts
+++ src/routes/users.routes.ts
@@ -1,7 +1,12 @@
 import { Router } from 'express';
+import multer from 'multer';
 import CreateUserService from '../services/CreateUserService';
+import UpdateAvatarService from '../services/UpdateUserAvatarService';
+import ensureAuthenticated from '../middlewares/ensureAuthenticated';
+import uploadCOnfig from '../config/upload';
 
 const usersRouter = Router();
+const upload = multer(uploadCOnfig);
 
 usersRouter.post('/', async (request, response) => {
   try {
@@ -18,4 +23,26 @@ usersRouter.post('/', async (request, response) => {
     return response.status(400).json({ error: error.message });
   }
 });
+
+usersRouter.patch(
+  '/avatar',
+  ensureAuthenticated,
+  upload.single('avatar'),
+  async (request, response) => {
+    try {
+      const updateUserAvatar = new UpdateAvatarService();
+
+      const user = await updateUserAvatar.execute({
+        user_id: request.user.id,
+        avatarFileName: request.file.filename,
+      });
+
+      delete user.password;
+      return response.json(user);
+    } catch (error) {
+      return response.status(400).json({ error: error.message });
+    }
+  },
+);
+
 export default usersRouter;
