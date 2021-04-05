6c13b972ae981ef2dc81bddd1f4f12c2dd10e800
Murillo ROmeu <murillosi@gmail.com>
2021-04-05 12:14:43 -0300

    add cors

--- src/server.ts
+++ src/server.ts
@@ -1,12 +1,14 @@
 /* eslint-disable no-console */
 import express, { Request, Response, NextFunction } from 'express';
 import 'express-async-errors';
+import cors from 'cors';
 import routes from './routes';
 import './database';
 import uploadConfig from './config/upload';
 import AppError from './errors/AppError';
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use('/files', express.static(uploadConfig.directory));
 app.use(routes);
